e1e57650e4f6144d6806671a28bbfb166ddaa192
matthewli <matthewli@10.16.85.34>
2018-05-11 09:43:16 -0700

    used enums rather than bools

--- Indomitable Will/src/processing/DrawingSurface.java
+++ Indomitable Will/src/processing/DrawingSurface.java
@@ -21,23 +21,23 @@ import java.util.Random;
 
 public class DrawingSurface extends PApplet {
 
-	private boolean howToPlay, haveDrawn;
-	private boolean levelStart;
+	
 	private Menu menu;
 	private Level testLevel;
 	private ArrayList<Integer> keys;
 	public static final int xSize = 1000;
 	public static final int ySize = 700;
-	private enum State {PAUSED, MENU, GAME, INSTRUCTIONS};
+
+	private enum State {
+		PAUSED, MENU, GAME, INSTRUCTIONS
+	};
+
 	private State state;
+
 	public DrawingSurface() {
 		keys = new ArrayList<Integer>();
 		testLevel = new Level();
 		menu = new Menu();
-		levelStart = false;
-		menu = new Menu();
-		howToPlay = false;
-		haveDrawn = false;
 		state = State.MENU;
 	}
 
@@ -52,13 +52,12 @@ public class DrawingSurface extends PApplet {
 
 	public void draw() {
 		pushStyle();
-		if (!haveDrawn) {
+		if (state == State.MENU) {
 			menu.draw(this);
-			haveDrawn = true;
 		}
-		if (levelStart == false) {
+		if (state != State.GAME) {
 			if (getMouseX() > width / 2 - 150 && getMouseX() < width / 2 + 150 && getMouseY() > height / 2 + 15
-					&& getMouseY() < height / 2 + 50 && mousePressed) {
+					&& getMouseY() < height / 2 + 50 && mousePressed && state == State.MENU) {
 
 				rect(0, 0, width, height);
 				rect(width - 150, height - 50, 100, 30);
@@ -68,15 +67,15 @@ public class DrawingSurface extends PApplet {
 				textSize(14);
 				text("This program is a top-down shooter game where the player represents the character Skal, whose mission is to save his village \nfrom an evil invasion. Skal can pick up multiple weapons, powerups, and other goodies to help aid his fight against the invaders. \nHe also can find a map with his progress. Skal has the ability to fight boss enemies, who have special abilities and are \nstronger than regular enemies.\n\nPress play to play the game. Skal should then enter rooms and defeat enemies to try to complete the game.",
 						100, 100);
-				howToPlay = true;
-			} else if (howToPlay && getMouseX() > width - 150 && getMouseX() < width - 50 && getMouseY() > height - 50
-					&& getMouseY() < height - 20 && mousePressed) {
-				howToPlay = false;
+				state = State.INSTRUCTIONS;
+			} else if (getMouseX() > width - 150 && getMouseX() < width - 50 && getMouseY() > height - 50
+					&& getMouseY() < height - 20 && mousePressed && state == State.INSTRUCTIONS) {
+				state = State.MENU;
 				background(255);
 				menu.draw(this);
-			} else if ( getMouseX() >width/2-200&& getMouseX() < width/2+200 && getMouseY() > height/2-115
-					&& getMouseY() < height /2 - 15&& mousePressed && !howToPlay) {
-				levelStart = true;
+			} else if (getMouseX() > width / 2 - 200 && getMouseX() < width / 2 + 200 && getMouseY() > height / 2 - 115
+					&& getMouseY() < height / 2 - 15 && mousePressed && state == State.MENU) {
+				state = State.GAME;
 			}
 		} else {
 			testLevel.draw(this);
@@ -85,12 +84,12 @@ public class DrawingSurface extends PApplet {
 			boolean up = keys.contains((int) 'W') || keys.contains(UP);
 			boolean left = keys.contains((int) 'A') || keys.contains(LEFT);
 			boolean right = keys.contains((int) 'D') || keys.contains(RIGHT);
-			
-//			stroke(0);
-//			textSize(100);
-//			System.out.println("HI");
-//			text("II", width - 300,height + 500);
-//			System.out.println("BAI");
+
+			// stroke(0);
+			// textSize(100);
+			// System.out.println("HI");
+			// text("II", width - 300,height + 500);
+			// System.out.println("BAI");
 			if (keys.contains((int) 'B')) {
 				testLevel.getPlayer().startFiring();
 			} else {
