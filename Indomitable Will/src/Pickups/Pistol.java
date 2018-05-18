498a6ea1d2ea4883e0e23e3e4bd753b8dc39ce41
nlao191 <nlao191@PR32.hhs.win.fuhsd.org>
2018-05-18 11:06:14 -0700

    added pistol class

--- Indomitable Will/src/Pickups/Pistol.java
+++ Indomitable Will/src/Pickups/Pistol.java
@@ -1,10 +1,30 @@
 package Pickups;
 
+import java.util.ArrayList;
+
 import processing.core.*;
 import sprites.Projectile;
-public class Pistol extends Weapon{
+
+/**
+ * 
+ * @author Nathaniel
+ * @version 5/18/18 11:03
+ */
+public class Pistol extends Weapon {
+	private final int BULLET_SPEED = 4;
+
 	public Pistol() {
 		ROF = 5;
 	}
+
+	public ArrayList<Projectile> fire(int xLoc, int yLoc, double angle) {
+		ArrayList<Projectile> result = new ArrayList<Projectile>();
+		result.add(new Projectile(xLoc, yLoc, Math.cos(Math.toRadians(angle)) * BULLET_SPEED,
+				Math.sin(Math.toRadians(angle)) * BULLET_SPEED));
+		return result;
+	}
 	
+	public Obtainable getDrop() {
+		return this;
+	}
 }
