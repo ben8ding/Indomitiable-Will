b3c6b6f8201225b5f2f01e6a5e493d3bcbd52776
Ben <ben8ding@gmail.com>
2018-05-18 19:44:57 -0700

    commit

--- Indomitable Will/src/Pickups/Rifle.java
+++ Indomitable Will/src/Pickups/Rifle.java
@@ -18,18 +18,10 @@ public class Rifle extends Weapon {
 
 		ArrayList<Projectile> fire = new ArrayList<Projectile>();
 
-<<<<<<< HEAD
+
 		fire.add(new Projectile(xLoc, yLoc, Math.cos(Math.toRadians(angle + 90)) * 10,
 				Math.sin(Math.toRadians(angle + 90)) * 10));
 		
-=======
-		fire.add(new Projectile(xLoc, yLoc, Math.cos(Math.toRadians(angle + 90)) * BULLET_SPEED,
-				Math.sin(Math.toRadians(angle + 90)) * BULLET_SPEED));
-		fire.add(new Projectile(xLoc, yLoc, Math.cos(Math.toRadians(angle + 90)) * BULLET_SPEED,
-				Math.sin(Math.toRadians(angle + 90)) * BULLET_SPEED));
-		fire.add(new Projectile(xLoc, yLoc, Math.cos(Math.toRadians(angle + 90)) * BULLET_SPEED,
-				Math.sin(Math.toRadians(angle + 90)) * BULLET_SPEED));
->>>>>>> branch 'Ben's_branch' of https://github.com/ben8ding/Indomitiable-Will.git
 
 		return fire;
 
