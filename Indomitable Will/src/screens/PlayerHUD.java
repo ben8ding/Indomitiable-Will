acae2a3d0a4ea547643667ecc3aff47af6958411
WarWzrd <natelao67@gmail.com>
2018-05-22 17:30:51 -0700

    added hud

--- Indomitable Will/src/screens/PlayerHUD.java
+++ Indomitable Will/src/screens/PlayerHUD.java
@@ -1,5 +1,17 @@
 package screens;
 
-public class PlayerHUD {
+import processing.core.PApplet;
+import sprites.Player;
 
+public class PlayerHUD {
+	private Player p;
+	public PlayerHUD(Player p) {
+		this.p = p;
+	}
+	public void setup(PApplet drawer) {
+		
+	}
+	public void draw() {
+		
+	}
 }
